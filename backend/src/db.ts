d508f036a28a1fc61a70fe4e40d8997a0bff89eb
hendro <hxndrxcode@gmail.com>
2025-11-05 23:04:27 +0700

    final

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,14 +1,6 @@
 import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
-// import dotenv from 'dotenv'
+import { database_url } from './config.js'
 
-// Load .env kalau lokal
-// if (!process.env.VERCEL && !process.env.CF_PAGES) {
-//   dotenv.config()
-// }
-
-const DATABASE_URL = process.env.DATABASE_URL
-if (!DATABASE_URL) throw new Error('DATABASE_URL is not defined')
-
-const sql = neon(DATABASE_URL)
+const sql = neon(database_url)
 export const db = drizzle(sql)
--- backend/src/local.ts
+++ backend/src/local.ts
@@ -1,6 +1,6 @@
 import { serve } from '@hono/node-server'
-import app from '../src/index.js';
-import { port } from '../src/config.js';
+import app from './index.js';
+import { port } from './config.js';
 
 serve({
   fetch: app.fetch,
