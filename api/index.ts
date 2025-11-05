c323bfad3375d5168a1e0f938c63a92e87f025ca
hendro <hxndrxcode@gmail.com>
2025-11-05 21:23:21 +0700

    cek 4

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,1 @@
-export const config = {
-  runtime: 'edge',
-}
-
-export default async function handler() {
-  return new Response(
-    JSON.stringify({
-      status: 'ok',
-      message: 'API dummy running from Vercel Edge',
-      time: new Date().toISOString(),
-    }),
-    {
-      headers: { 'Content-Type': 'application/json' },
-    }
-  )
-}
+export { default, config } from '../backend/adapters/vercel.ts'
