e449cd3b83ae158fb6b26e4d072a62be62267399
hendro <hxndrxcode@gmail.com>
2025-11-05 20:33:48 +0700

    comment env

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,9 +3,9 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import dotenv from 'dotenv'
 
 // Load .env kalau lokal
-if (!process.env.VERCEL && !process.env.CF_PAGES) {
-  dotenv.config()
-}
+// if (!process.env.VERCEL && !process.env.CF_PAGES) {
+//   dotenv.config()
+// }
 
 const DATABASE_URL = process.env.DATABASE_URL
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not defined')
--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 
 // Load .env hanya kalau dijalankan lokal
-if (!process.env.VERCEL && !process.env.CF_PAGES) {
-  dotenv.config()
-}
+// if (!process.env.VERCEL && !process.env.CF_PAGES) {
+//   dotenv.config()
+// }
 
 // Helper function agar lebih aman
 export const getEnv = (key: string, fallback?: string): string => {
