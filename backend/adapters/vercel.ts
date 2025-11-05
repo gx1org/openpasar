7adbbd0ec444e31f58b903ecd97b5d8512dcbe96
hendro <hxndrxcode@gmail.com>
2025-11-05 20:18:25 +0700

    update vercel adapter

--- backend/adapters/vercel.ts
+++ backend/adapters/vercel.ts
@@ -3,7 +3,7 @@ import app from '../src/index.js'
 
 // Gunakan default export, bukan GET/POST satu-satu
 export const config = {
-  runtime: 'edge', // atau 'nodejs' kalau kamu butuh modul Node
+  runtime: 'nodejs', // atau 'nodejs' kalau kamu butuh modul Node
 }
 
 export default handle(app)
